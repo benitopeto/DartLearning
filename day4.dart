d42e74dfd6f800c8f4cde332bbeb897303b3bb97
benitopeto <techlady120@gmail.com>
2025-06-24 19:59:17 +0300

    with day4

---
+++ day4.dart
@@ -0,0 +1,7 @@
+void main() {
+  String numStr = '42';
+  int number = int.parse(numStr);
+  print("The number converted is: $number");
+  print("And the answer is: $number");
+  print("The square of the number is ${number * number}");
+}
